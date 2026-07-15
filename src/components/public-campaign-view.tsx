1af19b6345f7b98e23fd1a3b3d337fb0e5392150
Chiến Vũ <chienvh@phx-smartschool.com>
2026-07-15 08:47:13 +0700

    Update public-campaign-view.tsx

--- src/components/public-campaign-view.tsx
+++ src/components/public-campaign-view.tsx
@@ -256,9 +256,5 @@ function transactionDateTime(value: string) {
     day: "2-digit",
     month: "2-digit",
     year: "numeric",
-    hour: "2-digit",
-    minute: "2-digit",
-    second: "2-digit",
-    hour12: false,
   }).format(new Date(value));
 }
